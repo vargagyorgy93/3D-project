f0cc010fb4c0b52cebdf6de797cd71238df288a6
vargapeter <omnis6@gmail.com>
2021-02-15 16:41:54 +0100

    b2

--- 3D/src/main/java/com/example/demo/Application.java
+++ 3D/src/main/java/com/example/demo/Application.java
@@ -15,15 +15,9 @@ public class Application {
 
 
 
-<<<<<<< HEAD
-        application.cheer("the 3D boiiiiiiiiiiis");
-        System.out.println(application.count());
-        System.out.println(application.greet("Varga György"));
-=======
         application.cheer("java");
-        System.out.println(application.count(5000));
+        System.out.println(application.count());
         System.out.println(application.greet("Greeting!"));
->>>>>>> cbe0ffffbe7d5fb9c6fe4f57ba7727eca664f726
 
     }
 
