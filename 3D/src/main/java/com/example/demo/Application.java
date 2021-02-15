a1e189ed0daabaa60ff626a1e4202e4cac4f970c
vargagyorgy93 <vargagyorgy93@gmail.com>
2021-02-15 16:06:23 +0100

    add greet

--- 3D/src/main/java/com/example/demo/Application.java
+++ 3D/src/main/java/com/example/demo/Application.java
@@ -7,7 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    SpringApplication.run(Application.class, args);
+    Application application = new Application();
+        System.out.println(application.greet());
     }
+
+    public String greet(){
+        return "Varga György";
+    }
+
 
 }
