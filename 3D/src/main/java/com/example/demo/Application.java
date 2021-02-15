de89428e88195d428e5574d7a020bd35409c1d7e
vargapeter <omnis6@gmail.com>
2021-02-15 16:08:12 +0100

    b1

--- 3D/src/main/java/com/example/demo/Application.java
+++ 3D/src/main/java/com/example/demo/Application.java
@@ -6,8 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
+        Application application = new Application();
+        System.out.println(application.count());
+    }
+
+    public Integer count (){
+        return 5000;
     }
 
 }
