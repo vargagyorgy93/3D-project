2be5f511f982f77e1bf9d95299e4301fed45b222
vargagyorgy93 <vargagyorgy93@gmail.com>
2021-02-15 16:44:56 +0100

    a3 nanana

--- 3D/src/main/java/com/example/demo/Application.java
+++ 3D/src/main/java/com/example/demo/Application.java
@@ -13,17 +13,14 @@ public class Application {
         SpringApplication.run(Application.class, args);
         Application application = new Application();
 
-
-
-<<<<<<< HEAD
         application.cheer("the 3D boiiiiiiiiiiis");
         System.out.println(application.count());
         System.out.println(application.greet("Varga György"));
-=======
+
         application.cheer("java");
-        System.out.println(application.count(5000));
+        System.out.println(application.count());
         System.out.println(application.greet("Greeting!"));
->>>>>>> cbe0ffffbe7d5fb9c6fe4f57ba7727eca664f726
+
 
     }
 
